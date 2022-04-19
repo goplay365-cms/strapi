6e6ab04038b662685747e6779ace66b1f1af7da7
Goplay365 <goplay365.cms@gmail.com>
2022-04-19 14:48:48 +0200

    TranslationConstant to Content

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 'use strict';
+const {
+  keepalives
+} = require("pg/lib/defaults");
+const translationsObj = require("../config/translations");
 
 module.exports = {
   /**
@@ -7,7 +11,7 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register( /*{ strapi }*/ ) {},
 
   /**
    * An asynchronous bootstrap function that runs before
@@ -16,5 +20,80 @@ module.exports = {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
-  bootstrap(/*{ strapi }*/) {},
+  bootstrap( /*{ strapi }*/ ) {
+
+    /**
+     * Ngarkimi i perkthimeve nga file ne db strapi
+     */
+
+    const obj = translationsObj.translations;
+
+    var j = 1;
+    var l = 1;
+    var m = false;
+
+    function loc(lan) {
+      switch (lan) {
+        case 'zweTranslation':
+          return 'en';
+        case 'albTranslation':
+          return 'sq';
+        case 'vatTranslation':
+          return 'it';
+        case 'venTranslation':
+          return 'es';
+        case 'tmpTranslation':
+          return 'pt-BR';
+        case 'yugTranslation':
+          return 'sr-Latn';
+        case 'luxTranslation':
+          return 'de';
+        case 'grcTranslation':
+          return 'el';
+        case 'vutTranslation':
+          return 'fr';
+        case 'mkdTranslation':
+          return 'mk';
+        default:
+          return 'en';
+      }
+    };
+
+    strapi.service('api::label.label').findOne(4137)
+      .then(function (uGjend) {
+        if (uGjend) {
+          m = true;
+        }
+      })
+      .then(function () {
+          if (!m) {
+            for (const o in obj) {
+              for (const k in obj[o]) {
+                if (k !== "messages") {
+                  strapi.service('api::label.label').create({
+                    data: {
+                      id: j++,
+                      keyword: k,
+                      translation: obj[o][k],
+                      locale: loc(o)
+                    }
+                  })
+                } else if (k == "messages") {
+                  for (const m in obj[o][k]) {
+                    strapi.service('api::message.message').create({
+                      data: {
+                        id: l++,
+                        keyword: m,
+                        translation: obj[o][k][m],
+                        locale: loc(o)
+                      }
+                    })
+                  }
+                }
+              }
+            }
+          }
+        }
+      );
+  },
 };
